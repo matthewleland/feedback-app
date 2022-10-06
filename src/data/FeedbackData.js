eced188428591048acc622d9e84c5c7fc05022da
Matthew Leland <mleland18@apu.edu>
2022-10-05 19:41:30 -0700

    section 3: components, props, & state

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Today at chick fil a my biscuit took over 10 minutes. I am furious and would like to speak to a director immediately. Who trains these useless workers??',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+]
+
+export default FeedbackData
